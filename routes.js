7040288d6bd2fb5533f096211d74ccfec172c32a
Melaniaift <melaniaiftinchi@gmail.com>
2024-10-18 11:40:24 +0300

    endpoints are working

--- routes.js
+++ routes.js
@@ -1,92 +1,140 @@
 // data validation language
 const Joi = require('joi');
 
+//library for mongodb management
+const mongoose = require('mongoose');
+
 // building the web server
 const express = require('express');
 const app = express();
 
+// Middleware to parse JSON request bodies
+app.use(express.json());
+
 const allowedStatuses = ['new', 'progress', 'testing', 'done'];
 
+// connection to the db
+mongoose.connect('mongodb://localhost/tasks')
+    .then(() => console.log('Connected to MongoDB...'))
+    .catch((err) => console.error('Could not connect to MongoDB', err))
+
+const taskSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    status: {
+        type: String,
+        enum: ['new', 'progress', 'testing', 'done'],
+        lowercase: true,
+        default: 'new',
+        required: true
+    },
+    description: {
+        type: String
+    }
+});
+
+const Task = mongoose.model('Task', taskSchema)
+
 // getting all tasks
-app.get('/api/tasks', (req, res) => {
-    getTasks().then((tasks) => {
-        res.send(tasks)
-    })
+app.get('/api/tasks', async (req, res) => {
+    const tasks = await Task.find();
+    res.send(tasks);
 })
 
 // getting a task
-app.get('/api/tasks/:id', (req, res) => {
-    const taskId = req.params.id;
-    getTask(taskId).then(task => {
-        if (!task) {
-            return res.status(404).send('The task with the given ID was not found')
-        }
-        res.send(task)
-    })
+app.get('/api/tasks/:id', async (req, res) => {
+    const task = await Task.find({ _id: req.params.id });
+
+    if (!task) {
+        return res.status(404).send('The task with the given ID was not found')
+    }
+    res.send(task)
 })
 
 // create a task
-app.post('/api/tasks/', (req, res) => {
+app.post('/api/tasks', async (req, res) => {
+
     const { error } = validateTask(req.body);
 
     if (error) {
-        return res.status(400).send(result.error.details[0].message)
+        return res.status(400).send(error.details[0].message)
     }
 
-    const taskName = req.body.name;
-    const taskDescription = req.body.description;
+    const task = new Task({
+        name: req.body.name,
+        description: req.body.description
+    })
 
-    const task = createTask(taskName, taskDescription);
+    // save the data in the db
+    try {
+        const result = await task.save();
+        console.log(result)
+    }
+    catch (ex) {
+        for (field in ex.errors)
+            console.log(ex.errors[field].message)
+    }
+
     res.send(task);
 })
 
 // update a task name and description
-app.put('/api/tasks/:id', (req, res) => {
-    const taskId = req.params.id;
+app.put('/api/tasks/:id', async (req, res) => {
     const { error } = validateTask(req.body);
 
     if (error) {
-        return res.status(400).send(result.error.details[0].message)
+        return res.status(400).send(error.details[0].message)
     }
 
-    const taskName = req.body.name;
-    const taskDescription = req.body.description;
-
-    const task = updateTask(taskId, taskName, taskDescription);
+    const task = await Task.findOneAndUpdate({ _id: req.params.id }, {
+        $set: {
+            name: req.body.name,
+            description: req.body.description
+        }
+    }, { new: true });
     res.send(task);
 });
 
-
 // update a task status
-app.put('/api/tasks/:id/status', (req, res) => {
-    const taskId = req.params.id;
-    const { error } = validateTask(req.body);
+app.put('/api/tasks/:id/status', async (req, res) => {
+    const { error } = validateStatus(req.body.status);
 
     if (error) {
-        return res.status(400).send(result.error.details[0].message)
+        return res.status(400).send(error.details[0].message)
     }
 
-    const taskStatus = req.body.status;
-    const task = updateTaskStatus(taskId, taskStatus)
+    const task = await Task.findOneAndUpdate({ _id: req.params.id }, {
+        $set: {
+            status: req.body.status
+        }
+    }, { new: true });
     res.send(task);
 });
 
 // delete task
-app.delete('/api/tasks/:id', (req, res) => {
-    const taskId = req.params.id;
-    res.send(removeTask(taskId));
+// return null if the id is not found
+app.delete('/api/tasks/:id', async (req, res) => {
+    const task = await Task.findByIdAndDelete(req.params.id);
+    res.send(task);
 })
 
 // validating task name and status
 function validateTask(task) {
     let schema = Joi.object({
         name: Joi.string().min(3).required(),
-        status: Joi.string().valid(...allowedStatuses).required(),
+        description: Joi.string().min(3)
     })
 
     return schema.validate(task);
 }
 
+function validateStatus(taskStatus) {
+    let status = Joi.string().valid(...allowedStatuses).required();
+    return status.validate(taskStatus);
+}
+
 // listen on a port, default is 3000 
 const port = process.env.PORT || 3000;
-app.listen(port, () => debug(`Listening on port ${port}...`));
+app.listen(port, () => console.log(`Listening on port ${port}...`));
